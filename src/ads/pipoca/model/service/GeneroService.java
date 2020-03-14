663b825fbce2fba8b98dcf5003f6e63395af605f
viniciusBotelho625 <viniciusoficialbotelho@outlook.com>
2020-03-14 19:08:52 -0300

    Ponte entre service e DAO

---
+++ src/ads/pipoca/model/service/GeneroService.java
@@ -0,0 +1,14 @@
+package ads.pipoca.model.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ads.pipoca.model.dao.GeneroDAO;
+import ads.pipoca.model.entity.Genero;
+
+public class GeneroService {
+	public ArrayList<Genero> listarGeneros() throws IOException {
+		GeneroDAO dao = new GeneroDAO();
+		return dao.listarGeneros();
+	}
+}
